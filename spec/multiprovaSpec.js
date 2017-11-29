247b8e6e021bd54e76e7c07d466db70e175cb02b
Paulo Sergio <Paulo Sergio>
2017-11-28 23:25:30 -0200

    jasmine configuration + required files

--- spec/multiprovaSpec.js
+++ spec/multiprovaSpec.js
@@ -1,12 +1,44 @@
 "use strict";
 
 describe('MultiprovaTest', function() {
-
+    // Inicializa o modulo angular para os testes
     beforeEach(module('provasOnline'));
+    
+    var $controller, $rootScope;
+  
+    /* antes de cada teste, injeta os atributos controller e escopo
+       para ficarem acessíveis dentro dos testes
+    */
+    beforeEach(inject(function(_$controller_, _$rootScope_) {
+      $controller = _$controller_;                  
+      $rootScope = _$rootScope_;
+    }));
 
-    describe("my test case", function() {
+    // Suíte de testes
+    describe("$scope.navigate", function() {
 		
-		it('should...', function() {});
+        // Caso de teste
+		it('should set the correct question to $scope', function() {
+          
+          var $scope = $rootScope.$new();
+          var controller = $controller('provaCtrl', { $scope: $scope });
+          
+          $scope.exam = ['test'];
+          var question = 0;
+          $scope.navigate(question);
+          expect($scope.currentQuestion).toEqual(0);
+        });
+      
+      it('should set the correct question to $scope', function() {
+          
+          var $scope = $rootScope.$new();
+          var controller = $controller('provaCtrl', { $scope: $scope });
+          
+          $scope.exam = ['test'];
+          var question = -1;
+          $scope.navigate(question);
+          expect($scope.currentQuestion).toEqual(-1);
+        });
 		
     });
-}
+})
