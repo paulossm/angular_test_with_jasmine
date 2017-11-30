ad2d04c349e22fbdef2777494d3c3d6c36b00eec
Paulo Sergio <Paulo Sergio>
2017-11-30 06:10:14 -0200

    tests over $scope.loadQuestion() completed

--- spec/multiprovaSpec.js
+++ spec/multiprovaSpec.js
@@ -4,25 +4,32 @@ describe('MultiprovaTest', function() {
     // Inicializa o modulo angular para os testes
     beforeEach(module('provasOnline'));
     
-    var $controller, $rootScope;
+    var $controller, $rootScope, $interval;
   
     /* antes de cada teste, injeta os atributos controller e escopo
        para ficarem acessíveis dentro dos testes
     */
-    beforeEach(inject(function(_$controller_, _$rootScope_) {
+    beforeEach(inject(function(_$controller_, _$rootScope_, _$interval_) {
         $controller = _$controller_;                  
         $rootScope = _$rootScope_;
+        $interval = _$interval_;
     }));
 
     // Suite de Testes
-    describe("the $scope.previousQuestion function", function() {
+    describe("the $scope.loadQuestion function", function() {
         
         /*
-            A função $scope.previousQuestion deve alterar o estado do objeto $scope.currentQuestion 
-            e decrementar em 1 caso a questão atual não seja a primeira.
+            A função $scope.loadQuestion deve carregar na tela o enunciado e alternativas 
+            da questão atual e ativa a contagem regressiva do tempo
+            com uma chamada a $scope.clockTicking;
             
             Requisitos: 
-                - o valor em $scope.currentQuestion deve ser maior que 0
+                Válidos:
+                    - O valor do parâmetro deve ser um número entre 0 e (n-1) sendo n o número de questões na prova. [1]
+                Inválidos:
+                    - O valor do parâmetro é menor que 0 [2]
+                    - O valor do parâmetro é maior que número de questões [3]
+                    - O valor do parâmetro é undefined [4]
 
         */
         
@@ -32,34 +39,88 @@ describe('MultiprovaTest', function() {
             $scope = $rootScope.$new();
             controller = $controller('provaCtrl', { $scope: $scope }); 
             
-            /* Mock objetos $scope.exam e $scope.currentQuestion que é consultado durante a execução do caso de teste */
-            $scope.exam = {length: ''};
+            /* Mock objetos consultados durante a execução do caso de teste */
+            $scope.exam = [];
             $scope.currentQuestion = 0;
+            $scope.clockTicking = true;
             
             /* Mocks de funções que são chamadas durante a execução do caso de teste */
-            $scope.loadQuestion = jasmine.createSpy('loadQuestion');
-            localStorage.setItem = jasmine.createSpy('setItem');
-        });
-      
-        
-        it('should decrement $scope.currentQuestion by 1, update localStorage and call to $scope.loadQuestion()', function() {            
-            $scope.exam.length = 10;
-            $scope.currentQuestion = 2;
-            $scope.previousQuestion();
-            
-            expect($scope.currentQuestion).toEqual(1);
-            expect(localStorage.setItem).toHaveBeenCalled();
-            expect($scope.loadQuestion).toHaveBeenCalled();
+            $scope.pauseTimer = jasmine.createSpy("pauseTimer");
+            $scope.resumeTimer = jasmine.createSpy("resumeTimer");
         });
         
-        it('should identify that $scope.currentQuestion is already the first question and do nothing', function() {
-            $scope.exam.length = 10;
-            $scope.currentQuestion = 0;
-            $scope.previousQuestion();
+        // Caso de teste que cobre classe (1)
+        it('should update enunciado, alternativas and letras when a valid question number is passed', function() {            
+            $scope.exam = [{
+                enunciado: 'enuciado-mock',
+                alternativas: 'alternativas-mock',
+                letraInstanciada: "a,b,c,d"
+            }];
+            $scope.answers = [{answer: 'b'}];
+            $scope.answer = {selectedOption: ''};
             
-            expect($scope.currentQuestion).toEqual(0);
-            expect(localStorage.setItem).not.toHaveBeenCalled();
-            expect($scope.loadQuestion).not.toHaveBeenCalled();
+            var question = 0;
+            $scope.loadQuestion(question);
+            
+            expect($scope.enunciado).toEqual($scope.exam[question].enunciado);
+            expect($scope.alternativas).toEqual($scope.exam[question].alternativas);
+            expect($scope.letras).toEqual($scope.exam[question].letraInstanciada.split(','));
+            expect($scope.answer.selectedOption).toEqual($scope.answers[question].answer);  
+            expect($scope.pauseTimer).toHaveBeenCalled();
+            expect($scope.resumeTimer).toHaveBeenCalled();
+        });
+        
+        // Caso de teste que cobre classe (2)
+        it('should not update enunciado, alternativas or letras when an invalid question number is passed', function() {            
+            $scope.exam = [];
+            $scope.answers = [];
+            $scope.answer = {selectedOption: 'N/A'};
+
+            var question = -1;
+            $scope.loadQuestion(question);
+            
+            expect($scope.enunciado).not.toBeDefined();
+            expect($scope.alternativas).not.toBeDefined();
+            expect($scope.letras).not.toBeDefined();
+            expect($scope.answer.selectedOption).toEqual('N/A');  
+            expect($scope.pauseTimer).not.toHaveBeenCalled();
+            expect($scope.resumeTimer).not.toHaveBeenCalled();
+        });
+        
+        // Caso de teste que cobre classe (3)
+        it('should not update enunciado, alternativas or letras when an invalid question number is passed [2]', function() {            
+            $scope.exam = [];
+            $scope.exam.length = 5;
+            $scope.answers = [];
+            $scope.answer = {selectedOption: 'N/A'};
+
+            var question = 6;
+            $scope.loadQuestion(question);
+            
+            expect($scope.enunciado).not.toBeDefined();
+            expect($scope.alternativas).not.toBeDefined();
+            expect($scope.letras).not.toBeDefined();
+            expect($scope.answer.selectedOption).toEqual('N/A');  
+            expect($scope.pauseTimer).not.toHaveBeenCalled();
+            expect($scope.resumeTimer).not.toHaveBeenCalled();
+        });
+        
+        // Caso de teste que cobre classe (4)
+        it('should not update enunciado, alternativas or letras when an undefined question number is passed', function() {            
+            $scope.exam = [];
+            $scope.exam.length = 5;
+            $scope.answers = [];
+            $scope.answer = {selectedOption: 'N/A'};
+
+            var question = undefined;
+            $scope.loadQuestion(question);
+            
+            expect($scope.enunciado).not.toBeDefined();
+            expect($scope.alternativas).not.toBeDefined();
+            expect($scope.letras).not.toBeDefined();
+            expect($scope.answer.selectedOption).toEqual('N/A');  
+            expect($scope.pauseTimer).not.toHaveBeenCalled();
+            expect($scope.resumeTimer).not.toHaveBeenCalled();
         });
     });
 })
--- src/provasOnline.app.js
+++ src/provasOnline.app.js
@@ -7,14 +7,47 @@ var app = angular.module('provasOnline', []);
 app.controller('provaCtrl', function($scope, $http, $interval, $timeout) {
     var exam = {};
     
-    $scope.previousQuestion = function () {
-        // Save answer for current question before loading the next one
-        // $scope.saveAnswer();
-        if ($scope.currentQuestion > 0) {
-            --$scope.currentQuestion;
-            exam['current-question'] = $scope.currentQuestion;
-            localStorage.setItem("exam-" + $scope.idEvento, JSON.stringify(exam));
-            $scope.loadQuestion($scope.currentQuestion);
+    $scope.loadQuestion = function (questionNumber) {
+        if(questionNumber == undefined || questionNumber < 0 || questionNumber >= $scope.exam.length) {
+            return;
+        }
+        
+        // pausar temporizador de prova
+        $scope.pauseTimer();
+
+        // Carregar enunciado
+        $scope.enunciado = $scope.exam[questionNumber].enunciado;
+
+        // Carregar Alternativas
+        $scope.alternativas = $scope.exam[questionNumber].alternativas;
+
+        // Carregar Letras Instanciadas
+        $scope.letras = $scope.exam[questionNumber].letraInstanciada.split(",");
+
+        /*
+         Configurar Objeto resposta para a questão atual
+         Limpa a alternativa marcada.
+         */
+        $scope.answer.selectedOption = 'N/R';
+        if ($scope.answers[questionNumber] != undefined) {
+            if ($scope.answers[questionNumber].answer != '') {
+                $scope.answer.selectedOption = $scope.answers[questionNumber].answer;
+            }
+        } else {
+            $scope.answer.selectedOption = "N/R";
+        }
+
+        // retoma o temporizador de prova
+        $scope.resumeTimer();
+    };
+    
+    $scope.pauseTimer = function() {
+        if($scope.clockTicking) {
+            $scope.clockTicking = $interval.cancel($scope.clockTicking);
         }
     };
+    
+    $scope.resumeTimer = function() {
+        $scope.clockTicking = $interval($scope.countDown, 1000);
+    }
 });
