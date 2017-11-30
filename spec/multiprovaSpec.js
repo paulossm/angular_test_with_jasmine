b1e37f870e4e6a6d7a6de4ba81d9f2b5001cb9a5
Paulo Sergio <Paulo Sergio>
2017-11-30 03:45:48 -0200

    tests over $scope.nextQuestion() completed

--- spec/multiprovaSpec.js
+++ spec/multiprovaSpec.js
@@ -14,17 +14,15 @@ describe('MultiprovaTest', function() {
         $rootScope = _$rootScope_;
     }));
 
-    // Suíte de testes
-    describe("the $scope.navigate function", function() {
+    // Suite de Testes
+    describe("the $scope.nextQuestion function", function() {
         
         /*
-            A função $scope.navigate deve alterar o estado do objeto $scope.currentQuestion em relação à questão a qual o aluno está visualizando.
-            Requisitos (Definição de classes de equivalência): 
-                Válidos:
-                    - A entrada deve ser um número inteiro positivo entre 0 e (n-1) sendo 'n' o número de questões da prova atual. [1]
-                Inválidos:
-                    - A entrada está fora do intervalo de questões [2]
-                    - A entrada é um valor undefined [3]
+            A função $scope.nextQuestion deve alterar o estado do objeto $scope.currentQuestion 
+            e incrementar em 1 caso a questão atual não seja a última.
+            
+            Requisitos: 
+                - o valor em $scope.currentQuestion deve estar entre 0 e (n-1) sendo 'n' o número de questões da prova atual
 
         */
         
@@ -35,50 +33,36 @@ describe('MultiprovaTest', function() {
             controller = $controller('provaCtrl', { $scope: $scope }); 
             
             /* Mock objetos $scope.exam e $scope.currentQuestion que é consultado durante a execução do caso de teste */
-            $scope.exam = {
-                length: '',
-            };
+            $scope.exam = {length: ''};
             $scope.currentQuestion = 0;
+            
             /* Mocks de funções que são chamadas durante a execução do caso de teste */
             $scope.loadQuestion = jasmine.createSpy('loadQuestion');
             localStorage.setItem = jasmine.createSpy('setItem');
+            $scope.finishExam = jasmine.createSpy('finishExam');
         });
       
-        // Caso de teste que cobre a classe (1)
-        it('should set the $scope.currentQuestion object to a valid value', function() {
-            /*
-                Como o intervalo de valores permitido é dinâmico, é preciso configurar o tamanho do array "$scope.exam" 
-                para ser consultado dentro da função e decidir se o valor de entrada é valido ou não.
-            */
+        
+        it('should increment $scope.currentQuestion by 1, update localStorage and call to $scope.loadQuestion()', function() {            
             $scope.exam.length = 10;
-            var question = 5;
-            $scope.navigate(question);
+            $scope.currentQuestion = 8;
+            $scope.nextQuestion();
             
-            expect($scope.currentQuestion).toEqual(5);
-            expect($scope.loadQuestion).toHaveBeenCalled();
+            expect($scope.currentQuestion).toEqual(9);
             expect(localStorage.setItem).toHaveBeenCalled();
+            expect($scope.loadQuestion).toHaveBeenCalled();
+            expect($scope.finishExam).not.toHaveBeenCalled();
         });
         
-        // Caso de teste que cobre a classe (2)
-        it('should not set the $scope.currentQuestion due to an invalid numeric input', function() {
-            $scope.exam.length = 3;
-            var question = 5;
-            $scope.navigate(question);
+        it('should identify that $scope.currentQuestion is already the last question and call $scope.finishExam()', function() {            
+            $scope.exam.length = 10;
+            $scope.currentQuestion = 9;
+            $scope.nextQuestion();
             
-            expect($scope.currentQuestion).not.toEqual(5);
-            expect($scope.loadQuestion).not.toHaveBeenCalled();
+            expect($scope.currentQuestion).toEqual(9);
             expect(localStorage.setItem).not.toHaveBeenCalled();
-        });
-        
-        // Caso de teste que cobre a classe (3)
-        it('should not set the $scope.currentQuestion due to an undefined input', function() {
-            $scope.exam.length = 3;
-            var question = undefined;
-            $scope.navigate(question);
-            expect($scope.currentQuestion).not.toEqual(undefined);
             expect($scope.loadQuestion).not.toHaveBeenCalled();
-            expect(localStorage.setItem).not.toHaveBeenCalled();
+            expect($scope.finishExam).toHaveBeenCalled();
         });
-		
     });
 })
--- src/provasOnline.app.js
+++ src/provasOnline.app.js
@@ -7,14 +7,16 @@ var app = angular.module('provasOnline', []);
 app.controller('provaCtrl', function($scope, $http, $interval, $timeout) {
     var exam = {};
     
-    $scope.navigate = function (questionNumber) {
-        if (questionNumber == undefined || questionNumber >= $scope.exam.length || questionNumber < 0) {
-            return;
-        } else {
-            $scope.currentQuestion = questionNumber;
+    $scope.nextQuestion = function () {
+        // Save answer for current question before loading the next one
+        // $scope.saveAnswer();
+        if ($scope.currentQuestion + 1 < $scope.exam.length) {
+            ++$scope.currentQuestion;
             exam['current-question'] = $scope.currentQuestion;
             localStorage.setItem("exam-" + $scope.idEvento, JSON.stringify(exam));
             $scope.loadQuestion($scope.currentQuestion);
+        } else {
+            $scope.finishExam();
         }
     };
 });
