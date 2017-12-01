f5ba72f2cf2146df62be1824ebc01ca8cd664111
Paulo Sergio <Paulo Sergio>
2017-11-30 23:18:47 -0200

    tests over .configClock completed

--- spec/multiprovaSpec.js
+++ spec/multiprovaSpec.js
@@ -4,32 +4,45 @@ describe('MultiprovaTest', function() {
     // Inicializa o modulo angular para os testes
     beforeEach(module('provasOnline'));
     
-    var $controller, $rootScope, $interval;
+    var $controller, $rootScope, $interval, $http, $timeout;
   
     /* antes de cada teste, injeta os atributos controller e escopo
        para ficarem acessíveis dentro dos testes
     */
-    beforeEach(inject(function(_$controller_, _$rootScope_, _$interval_) {
+    beforeEach(inject(function(_$controller_, _$rootScope_, _$interval_, _$http_, _$timeout_) {
         $controller = _$controller_;                  
         $rootScope = _$rootScope_;
         $interval = _$interval_;
+        $http = _$http_;
+        $timeout = _$timeout_;
     }));
 
     // Suite de Testes
-    describe("the $scope.loadQuestion function", function() {
+    describe("the $scope.configClock function", function() {
         
         /*
-            A função $scope.loadQuestion deve carregar na tela o enunciado e alternativas 
-            da questão atual e ativa a contagem regressiva do tempo
-            com uma chamada a $scope.clockTicking;
+            A função $scope.configClock é responsável por configurar o tempo de prova com base
+            no tempo definido pelo professor e o momento em que o aluno abre a janela para começar
+            a realizar a prova.
+            
+            Parâmetros:
+                - startTime: representa o horário que o aluno começou a prova.
+                - duration: tempo máximo para fazer a prova definido pelo professor.
+                - timeLeft: caso o aluno esteja voltando, este argumento possui o tempo restante do aluno para fazer a prova.
             
             Requisitos: 
                 Válidos:
-                    - O valor do parâmetro deve ser um número entre 0 e (n-1) sendo n o número de questões na prova. [1]
+                    - duration deve ser um valor no formato "hh:mm:ss" que representa um tempo. [1]
+                    - startTime deve ser uma data com hora (timestamp) ou ser undefined. [2]
+                    - timeLeft deve ser um objeto idêntico a $scope.time ou ser undefined. [3]
+                
                 Inválidos:
-                    - O valor do parâmetro é menor que 0 [2]
-                    - O valor do parâmetro é maior que número de questões [3]
-                    - O valor do parâmetro é undefined [4]
+                    - duration é um valor undefined [4]
+                    - duration não está no formato correto [5]
+                    - duration representa um tempo negativo [6]
+                    - duration representa um valor inválido e o acesso ao DOM também [7]
+                    - startTime está definido mas não representa uma data com hora. [8]
+                    - timeLeft está definido mas não é um objeto $scope.time [9]
 
         */
         
@@ -40,87 +53,164 @@ describe('MultiprovaTest', function() {
             controller = $controller('provaCtrl', { $scope: $scope }); 
             
             /* Mock objetos consultados durante a execução do caso de teste */
-            $scope.exam = [];
-            $scope.currentQuestion = 0;
-            $scope.clockTicking = true;
+            $scope.exam = {};            
             
             /* Mocks de funções que são chamadas durante a execução do caso de teste */
-            $scope.pauseTimer = jasmine.createSpy("pauseTimer");
-            $scope.resumeTimer = jasmine.createSpy("resumeTimer");
+            localStorage.setItem = jasmine.createSpy("setItem");
+            document.getElementById = jasmine.createSpy("DOM").and.returnValue({value: "01:01:01"});
         });
         
+        
         // Caso de teste que cobre classe (1)
-        it('should update enunciado, alternativas and letras when a valid question number is passed', function() {            
-            $scope.exam = [{
-                enunciado: 'enuciado-mock',
-                alternativas: 'alternativas-mock',
-                letraInstanciada: "a,b,c,d"
-            }];
-            $scope.answers = [{answer: 'b'}];
-            $scope.answer = {selectedOption: ''};
+        it('should set $scope.startTime and $scope.duration and calculate time left', function() { 
+            var startTime = new Date();
+            var duration = "01:01:01";
+            var timeLeft = undefined;
             
-            var question = 0;
-            $scope.loadQuestion(question);
+            $scope.configClock(startTime, duration, timeLeft);
             
-            expect($scope.enunciado).toEqual($scope.exam[question].enunciado);
-            expect($scope.alternativas).toEqual($scope.exam[question].alternativas);
-            expect($scope.letras).toEqual($scope.exam[question].letraInstanciada.split(','));
-            expect($scope.answer.selectedOption).toEqual($scope.answers[question].answer);  
-            expect($scope.pauseTimer).toHaveBeenCalled();
-            expect($scope.resumeTimer).toHaveBeenCalled();
+            expect($scope.exam.duration).toEqual(duration);
+            expect($scope.examTime.startTime.getTime()).toEqual(startTime.getTime());
+            expect($scope.examTime.endTime).toBeGreaterThan($scope.examTime.startTime);
+            expect($scope.time.hours.left).toEqual(parseInt('01'));
+            expect($scope.time.minutes.left).toEqual(parseInt('01'));
+            expect($scope.time.seconds.left).toEqual(parseInt('01'));
+            
+            expect(document.getElementById).not.toHaveBeenCalled();
+            expect(localStorage.setItem).toHaveBeenCalled();
         });
         
         // Caso de teste que cobre classe (2)
-        it('should not update enunciado, alternativas or letras when an invalid question number is passed', function() {            
-            $scope.exam = [];
-            $scope.answers = [];
-            $scope.answer = {selectedOption: 'N/A'};
-
-            var question = -1;
-            $scope.loadQuestion(question);
+        it('should create $scope.startTime and calculate time left', function() { 
+            var startTime = undefined;
+            var duration = "01:01:01";
+            var timeLeft = undefined;
             
-            expect($scope.enunciado).not.toBeDefined();
-            expect($scope.alternativas).not.toBeDefined();
-            expect($scope.letras).not.toBeDefined();
-            expect($scope.answer.selectedOption).toEqual('N/A');  
-            expect($scope.pauseTimer).not.toHaveBeenCalled();
-            expect($scope.resumeTimer).not.toHaveBeenCalled();
+            $scope.configClock(startTime, duration, timeLeft);
+            
+            expect($scope.exam.duration).toEqual(duration);
+            expect($scope.examTime.startTime.getTime()).toBeDefined();
+            expect($scope.examTime.endTime).toBeGreaterThan($scope.examTime.startTime);
+            expect($scope.time.hours.left).toEqual(parseInt('01'));
+            expect($scope.time.minutes.left).toEqual(parseInt('01'));
+            expect($scope.time.seconds.left).toEqual(parseInt('01'));
+            
+            expect(document.getElementById).not.toHaveBeenCalled();
+            expect(localStorage.setItem).toHaveBeenCalled();
         });
         
         // Caso de teste que cobre classe (3)
-        it('should not update enunciado, alternativas or letras when an invalid question number is passed [2]', function() {            
-            $scope.exam = [];
-            $scope.exam.length = 5;
-            $scope.answers = [];
-            $scope.answer = {selectedOption: 'N/A'};
-
-            var question = 6;
-            $scope.loadQuestion(question);
+        it('should create $scope.startTime and use time left from arguments', function() { 
+            var startTime = undefined;
+            var duration = "1:00:00";
+            var timeLeft = {
+                'hours': {'left': 0},
+                'minutes': {'left': 15},
+                'seconds': {'left': 0}
+            };
             
-            expect($scope.enunciado).not.toBeDefined();
-            expect($scope.alternativas).not.toBeDefined();
-            expect($scope.letras).not.toBeDefined();
-            expect($scope.answer.selectedOption).toEqual('N/A');  
-            expect($scope.pauseTimer).not.toHaveBeenCalled();
-            expect($scope.resumeTimer).not.toHaveBeenCalled();
+            $scope.configClock(startTime, duration, timeLeft);
+            
+            expect($scope.exam.duration).toEqual(duration);
+            expect($scope.examTime.startTime.getTime()).toBeDefined();
+            expect($scope.examTime.endTime).toBeGreaterThan($scope.examTime.startTime);
+            expect($scope.time.hours.left).toEqual(timeLeft.hours.left);
+            expect($scope.time.minutes.left).toEqual(timeLeft.minutes.left);
+            expect($scope.time.seconds.left).toEqual(timeLeft.seconds.left);
+            
+            expect(document.getElementById).not.toHaveBeenCalled();
+            expect(localStorage.setItem).toHaveBeenCalled();
         });
         
         // Caso de teste que cobre classe (4)
-        it('should not update enunciado, alternativas or letras when an undefined question number is passed', function() {            
-            $scope.exam = [];
-            $scope.exam.length = 5;
-            $scope.answers = [];
-            $scope.answer = {selectedOption: 'N/A'};
-
-            var question = undefined;
-            $scope.loadQuestion(question);
+        it('should access DOM to retrieve duration information', function() { 
+            document.getElementById = jasmine.createSpy("DOM").and.returnValue({value: "01:01:01"});
+            var startTime = undefined;
+            var duration = undefined;
+            var timeLeft = undefined;
             
-            expect($scope.enunciado).not.toBeDefined();
-            expect($scope.alternativas).not.toBeDefined();
-            expect($scope.letras).not.toBeDefined();
-            expect($scope.answer.selectedOption).toEqual('N/A');  
-            expect($scope.pauseTimer).not.toHaveBeenCalled();
-            expect($scope.resumeTimer).not.toHaveBeenCalled();
+            $scope.configClock(startTime, duration, timeLeft);
+            
+            expect($scope.exam.duration).toEqual("01:01:01");
+            expect($scope.examTime.startTime.getTime()).toBeDefined();
+            expect($scope.examTime.endTime).toBeGreaterThan($scope.examTime.startTime);
+            expect($scope.time.hours.left).toEqual(parseInt("01"));
+            expect($scope.time.minutes.left).toEqual(parseInt("01"));
+            expect($scope.time.seconds.left).toEqual(parseInt("01"));
+            
+            expect(document.getElementById).toHaveBeenCalled();
+            expect(localStorage.setItem).toHaveBeenCalled();
+        });
+        
+        // Caso de teste que cobre classe (5)
+        it('should access DOM to retrieve duration information due to an invalid input', function() { 
+            document.getElementById = jasmine.createSpy("DOM").and.returnValue({value: "01:01:01"});
+            var startTime = undefined;
+            var duration = "1h30m0s";
+            var timeLeft = undefined;
+            
+            $scope.configClock(startTime, duration, timeLeft);
+            
+            expect($scope.exam.duration).toEqual("01:01:01");
+            expect(document.getElementById).toHaveBeenCalled();
+            expect(localStorage.setItem).toHaveBeenCalled();
+        });
+        
+        // Caso de teste que cobre classe (6)
+        it('should access DOM to retrieve duration information due to an invalid input', function() { 
+            document.getElementById = jasmine.createSpy("DOM").and.returnValue({value: "01:01:01"});
+            var startTime = undefined;
+            var duration = "-3:30:20";
+            var timeLeft = undefined;
+            
+            $scope.configClock(startTime, duration, timeLeft);
+            
+            expect($scope.exam.duration).toEqual("01:01:01");
+            expect(document.getElementById).toHaveBeenCalled();
+            expect(localStorage.setItem).toHaveBeenCalled();
+        });
+        
+        // Caso de teste que cobre classe (7)
+        it('should not perform any action because both duration parameter and DOM are undefined', function() { 
+            document.getElementById = jasmine.createSpy("DOM").and.returnValue({value: undefined});
+            var startTime = undefined;
+            var duration = undefined;
+            var timeLeft = undefined;
+            
+            $scope.configClock(startTime, duration, timeLeft);
+            
+            expect($scope.exam.duration).not.toBeDefined();
+            expect(document.getElementById).toHaveBeenCalled();
+            expect(localStorage.setItem).not.toHaveBeenCalled();
+        });
+        
+        // Caso de teste que cobre classe (8)
+        it('should create a new Date object for startTime because the parameter is invalid', function() { 
+            var startTime = "2017, 10 Fev";
+            var compareDate = new Date("2017-02-10");
+            var duration = "01:01:01";
+            var timeLeft = undefined;
+            
+            $scope.configClock(startTime, duration, timeLeft);
+            
+            expect($scope.examTime.startTime.getTime()).toBeGreaterThan(compareDate.getTime());
+            expect(document.getElementById).not.toHaveBeenCalled();
+            expect(localStorage.setItem).toHaveBeenCalled();
+        });
+        
+        // Caso de teste que cobre classe (9)
+        it('should create a new $scope.time object because timeLeft parameter is invalid', function() { 
+            var startTime = undefined;
+            var duration = "01:00:00";
+            var timeLeft = {};
+            
+            $scope.configClock(startTime, duration, timeLeft);
+            
+            expect($scope.time.hours.left).toEqual(parseInt("01"));
+            expect($scope.time.minutes.left).toEqual(parseInt("00"));
+            expect($scope.time.seconds.left).toEqual(parseInt("00"));
+            expect(document.getElementById).not.toHaveBeenCalled();
+            expect(localStorage.setItem).toHaveBeenCalled();
         });
     });
 })
--- src/provasOnline.app.js
+++ src/provasOnline.app.js
@@ -7,47 +7,71 @@ var app = angular.module('provasOnline', []);
 app.controller('provaCtrl', function($scope, $http, $interval, $timeout) {
     var exam = {};
     
-    $scope.loadQuestion = function (questionNumber) {
-        if(questionNumber == undefined || questionNumber < 0 || questionNumber >= $scope.exam.length) {
-            return;
+    $scope.configClock = function (startTime, duration, timeLeft) {
+
+        $scope.timeIsOver = false;
+        
+        if (startTime && startTime.getTime) {
+            var now = startTime;
+        } else {
+            now = new Date();
+            exam['start-time'] = now.toISOString();   
         }
         
-        // pausar temporizador de prova
-        $scope.pauseTimer();
-
-        // Carregar enunciado
-        $scope.enunciado = $scope.exam[questionNumber].enunciado;
-
-        // Carregar Alternativas
-        $scope.alternativas = $scope.exam[questionNumber].alternativas;
-
-        // Carregar Letras Instanciadas
-        $scope.letras = $scope.exam[questionNumber].letraInstanciada.split(",");
-
-        /*
-         Configurar Objeto resposta para a questão atual
-         Limpa a alternativa marcada.
-         */
-        $scope.answer.selectedOption = 'N/R';
-        if ($scope.answers[questionNumber] != undefined) {
-            if ($scope.answers[questionNumber].answer != '') {
-                $scope.answer.selectedOption = $scope.answers[questionNumber].answer;
-            }
+        var regex = /^(([0-9]|0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$)+/;
+        if (duration && regex.test(duration)) {
+            $scope.exam.duration = duration;
         } else {
-            $scope.answer.selectedOption = "N/R";
+            duration = document.getElementById("examDuration").value;
+            if(regex.test(duration)) {
+                $scope.exam.duration = duration;
+                exam['duration'] = $scope.exam.duration;    
+            } else {
+                return;
+            }
         }
+        
+        var final = new Date(now.getTime());
+        var examTime = $scope.exam.duration.split(":");
+        var hours = examTime[0];
+        var minutes = examTime[1];
+        var seconds = examTime[2];
+        
+        final.setHours(final.getHours() + parseInt(hours));
+        final.setMinutes(final.getMinutes() + parseInt(minutes));
+        final.setSeconds(final.getSeconds() + parseInt(seconds));
+        
+        $scope.examTime = {
+            'startTime': now,
+            'endTime': final
+        };
+        
+        if (timeLeft && timeLeft.hours && timeLeft.minutes && timeLeft.seconds) {
+            $scope.time = timeLeft;
+        } else {
+            $scope.time = {
+                'hours': {
+                    'left': ''
+                },
+                'minutes': {
+                    'left': ''
+                },
+                'seconds': {
+                    'left': ''
+                }
+            };
+            
+            var totalTime = ($scope.examTime.endTime - $scope.examTime.startTime);
+            var totalInSeconds = Math.round(totalTime / 1000);
+            $scope.time.hours.left = Math.floor(totalInSeconds / 3600);
+            totalInSeconds -= ($scope.time.hours.left * 3600);
+            $scope.time.minutes.left = Math.floor(totalInSeconds / 60);
+            $scope.time.seconds.left = totalInSeconds - ($scope.time.minutes.left * 60);
 
-        // retoma o temporizador de prova
-        $scope.resumeTimer();
-    };
-    
-    $scope.pauseTimer = function() {
-        if($scope.clockTicking) {
-            $scope.clockTicking = $interval.cancel($scope.clockTicking);
+            exam['time-left'] = $scope.time;
         }
+        
+        localStorage.setItem("exam-" + $scope.idEvento, JSON.stringify(exam));
     };
     
-    $scope.resumeTimer = function() {
-        $scope.clockTicking = $interval($scope.countDown, 1000);
-    }
 });
