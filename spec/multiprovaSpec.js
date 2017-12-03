a6bc911f5763e600473980d3729c3354adb52e7a
Paulo Sergio <Paulo Sergio>
2017-12-03 19:32:01 -0200

    Append unit test #07

--- spec/multiprovaSpec.js
+++ spec/multiprovaSpec.js
@@ -595,4 +595,104 @@ describe('MultiprovaTest', function() {
             expect(localStorage.setItem).toHaveBeenCalled();
         });
     });
+    
+    describe("the $scope.configExam function", function() {
+        
+        /*
+            Esta função é responsável por inicializar as variáveis com dados da prova
+            que está sendo feita. Ela recebe um objeto data que é resultado de uma requisição
+            da aplicação a um serviço externo, e então preenche as variáveis necessárias.
+            
+            Requisitos:
+                Válidos:
+                    - data['previews'] e data['idProva']['idProvaInstanciada'] precisam estar definidos [1]
+                    - data["current-question"] pode ser indefinido ou um número entre 0 e (n-1) sendo 'n' o número de questões da prova atual[2]
+                    - data["answers"] pode ser indefinido ou representar um array de mesmo tamanho do número de questões da prova atual. [3]
+                    
+                Inválidos:
+                    - data está indefinido [4]
+                    - data não está no formato válido [5]
+        */
+        
+        var $scope, controller;
+      
+        beforeEach(function() {
+            /* Mock objetos consultados durante a execução do caso de teste */
+            this.data = {
+                previews: [],
+                idProva: {idProvaInstanciada: ''},
+                'current-question': '',
+                answers: [],
+                
+                mockFullData: function() {
+                    this.previews = ['m','o','c','k'];
+                    this.idProva.idProvaInstanciada = 1;
+                    this['current-question'] = 1;
+                    this.answers = ['a','b', 'c', 'd'];
+                }
+            };
+            
+            $scope = $rootScope.$new();
+            controller = $controller('provaCtrl', { $scope: $scope }); 
+            
+            /* Mocks de funções que são chamadas durante a execução do caso de teste */
+            localStorage.setItem = jasmine.createSpy("setItem");
+        });
+        
+        
+        // Caso de teste que cobre classe (1)
+        it('should assign previews and idProva correctly', function() { 
+            this.data.previews = ['m','o','c','k'];
+            this.data.idProva.idProvaInstanciada = 1;
+            
+            $scope.configExam(this.data);
+            
+            expect($scope.exam).toEqual(this.data.previews);
+            expect($scope.idProva).toEqual(this.data.idProva.idProvaInstanciada);
+            expect(localStorage.setItem).toHaveBeenCalled();
+        });
+        
+        // Caso de teste que cobre classe (2)
+        it('should assign current-question correctly', function() { 
+            this.data.previews = ['m','o','c','k'];
+            this.data.idProva.idProvaInstanciada = 1;
+            this.data['current-question'] = '2';
+            
+            $scope.configExam(this.data);
+            
+            expect($scope.currentQuestion).toEqual(parseInt(this.data['current-question']));
+            expect(localStorage.setItem).not.toHaveBeenCalled();
+        });
+        
+         // Caso de teste que cobre classe (3)
+        it('should assign correct data to $scope variables', function() { 
+            this.data.mockFullData();
+            
+            $scope.configExam(this.data);
+            
+            expect($scope.answers).toEqual(this.data.answers);
+            expect(localStorage.setItem).not.toHaveBeenCalled();
+        });
+        
+        // Caso de teste que cobre classe (4)
+        it('should not perform action due to undefined data input', function() { 
+            this.data = undefined;
+            
+            $scope.configExam(this.data);
+            
+            expect($scope.exam).not.toBeDefined();
+            expect(localStorage.setItem).not.toHaveBeenCalled();
+        });
+        
+        // Caso de teste que cobre classe (5)
+        it('should not perform action due to invalid data input format', function() { 
+            this.data = ['myData'];
+            
+            $scope.configExam(this.data);
+            
+            expect($scope.exam).not.toBeDefined();
+            expect(localStorage.setItem).not.toHaveBeenCalled();
+        });
+        
+    });
 })
--- src/provasOnline.app.js
+++ src/provasOnline.app.js
@@ -186,4 +186,40 @@ app.controller('provaCtrl', function($scope, $http, $interval, $timeout) {
         exam['time-left'] = $scope.time;
         localStorage.setItem("exam-" + $scope.idEvento, JSON.stringify(exam))
     };
+    
+    $scope.configExam = function (data) {
+        if(!data || !data.previews || !data.idProva) {
+            $("body").addClass("notLoaded");
+            return;
+        }
+        
+        $scope.exam = data['previews'];
+        $scope.idProva = data['idProva']['idProvaInstanciada'];
+
+        if (data["current-question"]) {
+            $scope.currentQuestion = parseInt(data["current-question"]);
+        } else {
+            $scope.currentQuestion = 0;
+            data['current-question'] = 0;
+            localStorage.setItem("exam-" + $scope.idEvento, JSON.stringify(data));
+        }
+
+        $scope.totalQuestions = $scope.exam.length;
+        $scope.answer = {selectedOption: 'N/R'};
+        if (data["answers"]) {
+            $scope.answers = data["answers"];
+        } else {
+            $scope.answers = new Array($scope.exam.length);
+            /* fill answer sheet */
+            for (var i = 0; i < $scope.answers.length; i++) {
+                $scope.answers[i] = {
+                    "question": i + 1,
+                    "answer": 'N/R'
+                }
+            }
+            data['answers'] = $scope.answers;
+            localStorage.setItem("exam-" + $scope.idEvento, JSON.stringify(data));
+        }
+        $scope.letter = String.fromCharCode(97);
+    };
 });
